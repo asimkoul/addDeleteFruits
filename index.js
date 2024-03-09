e8ad24dca99bbd4f0884fd6402de91da5ad07dab
asimkoul <ahmadasim080gmail.com>
2024-03-09 17:54:31 +0530

    searchFunc

--- index.js
+++ index.js
@@ -30,3 +30,20 @@ fruits.addEventListener("click",function (event){
         fruits.removeChild(fruitToDel)
     }
 })
+// search funx
+const filter=document.getElementById("filter")
+filter.addEventListener("keyup",function(event){
+    const textEntered= event.target.value.toLowerCase();
+    const fruitItems= document.getElementsByClassName("fruit")
+    for(let i=0;i<fruitItems.length;i++){
+        const currentFruitText=fruitItems[i].firstChild.textContent.toLowerCase();
+        if(currentFruitText.indexOf(textEntered)==-1){
+            fruitItems[i].style.display="none"
+        }
+        else{
+            fruitItems[i].style.display="flex"
+
+        }
+        }
+    }
+)
